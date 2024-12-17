aea5859c0edd1b3b9e5995e1ca921fe7cf809d33
bazuly <serejka50@gmail.com>
2024-12-17 17:30:05 +0300

    remove rating test added

--- apps/library/tests/test_library.py
+++ apps/library/tests/test_library.py
@@ -109,3 +109,16 @@ class TestLibraryAPI:
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
         assert 'You already set rating' in response.data['non_field_errors']
+
+    def test_remove_rating(self):
+        rating = BookRatingModel.objects.create(
+            id=1,
+            user=self.user,
+            book=self.book,
+            rating=5
+        )
+        url = reverse('remove_rating', kwargs={'pk': rating.id})
+        response = self.client.delete(url)
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        assert not BookRatingModel.objects.filter(id=rating.id).exists()
