1373dc6475518e98956ccec54a1f5400c32022a7
bazuly <serejka50@gmail.com>
2024-12-20 15:52:42 +0300

    rabbit no tests yet

--- hard_train/celery.py
+++ hard_train/celery.py
@@ -5,9 +5,11 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hard_train.settings')
 
 
-app = Celery('hard_train', backend='redis://6379/0')
+app = Celery('hard_train')
+
+app.conf.broker_url = 'amqp://guest:guest@rabbitmq:5672/'
+app.conf.result_backend = 'redis://redis:6379/0'
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
 
 app.autodiscover_tasks()
--- hard_train/settings.py
+++ hard_train/settings.py
@@ -136,7 +136,7 @@ REDIS_PORT = 6379
 
 
 # celery settings
-CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_BROKER_URL = "amqp://guest:guest@rabbitmq:5672/"
 CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
